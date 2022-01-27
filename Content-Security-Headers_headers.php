691186d3d82ad5204b8e65d58d220370287e43c4
MatthewBuchananAstley <matthewbuchanan@astley.nl>
2022-01-27 21:49:40 +0100

    Added Content-Security-Policy headers file and README text

---
+++ Content-Security-Headers_headers.php
@@ -0,0 +1,15 @@
+<?php
+#
+#
+# Cross site scripting mitigation
+#
+# https://content-security-policy.com/
+# https://csp.withgoogle.com/docs/index.html
+# https://csp-evaluator.withgoogle.com/
+#
+
+header("Content-Security-Policy: script-src 'none'; object-src 'none';base-uri 'none'; require-trusted-types-for 'script'; frame-ancestors: 'none'") ;
+header("X-Content-Type-Options: nosniff");
+header("Strict-Transport-Security: max-age=31536000; includeSubDomains");
+
+?>
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # php
 php code
+
+# Content-Security-Headers_headers.php 
+
+Set Contect-Security-Policy headers so that vulnerability scanners refrain from complaining.  
+
+check your site with:
+https://csp-evaluator.withgoogle.com/
+
+https://content-security-policy.com/
+https://csp.withgoogle.com/docs/index.html
+
